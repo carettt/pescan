43659ea968d7421219af2e88776106496fa93ccf
caret_ <dandradepac@outlook.com>
2025-03-20 21:22:57 +0000

    Initial commit, including dev shell, linux package builder, and docker image biulder

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "rust project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.cargo
+            pkgs.rustc
+          ];
+
+          shellHook = ''
+            ${pkgs.cowsay}/bin/cowsay "entered dev env!" | ${pkgs.lolcat}/bin/lolcat -F 0.5
+          '';
+        };
+
+        packages = rec {
+          pescan = let
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+          in pkgs.rustPlatform.buildRustPackage {
+            pname = manifest.name;
+            version = manifest.version;
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+          };
+
+          dockerImage = pkgs.dockerTools.buildLayeredImage {
+            name = "pescan";
+            tag = "latest";
+            created = "2025-03-20";
+
+            contents = [
+              pescan
+            ];
+
+            config.Cmd = [ "/bin/pescan" ];
+          };
+        };
+      });
+}
