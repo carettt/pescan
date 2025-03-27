4777ba9209297c4f491e68fe11906da344e9ee46
caret_ <dandradepac@outlook.com>
2025-03-27 22:48:09 +0000

    Add documentation and get basic functionality working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![warn(missing_docs)]
+
+//! PEScan is a malware analysis tool that scans portable executable (PE)
+//! files for potentially malicious Windows API imports.
+//! The program uses [HashSet](std::collections::hash_set::HashSet)s for
+//! maximum efficiency in comparing import lists.
+//! The binary can output in multiple formats and provide
+//! a potential attack chain for the sample.
+
 use clap::Parser;
 use anyhow::{Result, anyhow, bail};
 use camino::Utf8PathBuf;
@@ -5,41 +14,45 @@ use goblin::{Object, pe::import::Import};
 use scraper::{Html, Selector};
 
 use std::fs;
+use std::collections::hash_set::HashSet;
 
+
+/// pescan - static analysis tool for PE files via API import analysis
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-  file: Utf8PathBuf
+  file: Utf8PathBuf,
+
+  #[arg(short, long)]
+  description: bool
 }
 
+
+/// [FlatImport] is a utility struct to flatten the PE imports returned by the [goblin] crate
 #[derive(Debug)]
 struct FlatImport {
   name: String,
   dll: String
 }
 
+/// Flattens [goblin::pe::import::Import] into [FlatImport]
 fn flatten_imports(raw_imports: &Vec<Import>) -> Vec<FlatImport> {
-  return raw_imports.iter().map(|i| FlatImport { name: i.name.to_string(), dll: i.dll.to_string() }).collect();
+  return raw_imports.iter()
+    .map(|i| FlatImport { name: i.name.to_string(), dll: i.dll.to_string() }).collect();
 }
 
+/// Scrapes headers from HTML fetched from <https://malapi.io>
 fn get_headers(document: &Html) -> Result<Vec<String>> {
   let header_selector = Selector::parse("th")
     .map_err(|e| anyhow!("failed to parse selector: {e}"))?;
 
   let headers: Vec<String> = document.select(&header_selector)
-    .map(|element| {
-      let mut inner_html = element.inner_html();
-
-      if let Some((before, _)) = inner_html.split_once(' ') {
-        inner_html = before.to_string();
-      }
-
-      inner_html
-    }).collect();
+    .map(|element| element.text().map(|s| s.trim()).collect()).collect();
 
   Ok(headers)
 }
 
+/// Scrapes APIs from HTML fetched from <https://malapi.io>
 fn get_apis(document: &Html) -> Result<Vec<Vec<String>>> {
   let mut apis: Vec<Vec<String>> = Vec::new();
   let column_selector = Selector::parse("td > table > tbody")
@@ -64,15 +77,32 @@ fn main() -> Result<()> {
   let sample_html = fs::read_to_string(Utf8PathBuf::from("../source.html"))?;
   let document  = Html::parse_document(&sample_html);
   let headers = get_headers(&document)?;
-  let apis = get_apis(&document);
-
-  println!("{:#?}", headers);
-  println!("{:#?}", apis);
+  let apis = get_apis(&document)?;
 
   match Object::parse(&file_buffer)? {
     Object::PE(pe) => {
       let imports = flatten_imports(&pe.imports);
+      let mut suspicious_imports = Vec::<Vec<String>>::new();
+
       std::mem::drop(pe);
+
+      for category in apis.iter() {
+        let category_set = category.iter().cloned().collect::<HashSet<String>>();
+        let import_set = imports.iter().map(|i| i.name.clone()).collect::<HashSet<String>>();
+
+        suspicious_imports.push(category_set.intersection(&import_set).cloned().collect());
+      }
+
+      for (i, header) in headers.iter().enumerate() {
+        if !suspicious_imports[i].is_empty() {
+          println!("{header}: [");
+          for import in &suspicious_imports[i] {
+            println!("\t{import},");
+          }
+          println!("]");
+        }
+      }
+
     },
     _ => {
       bail!("invalid file type, only PE files are supported");
