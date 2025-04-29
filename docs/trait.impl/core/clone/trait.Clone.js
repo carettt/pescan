875c235e30065cc465a6f1f659f15882fd0e5a92
caret_ <dandradepac@outlook.com>
2025-04-29 02:10:54 +0100

    Update documentation

--- docs/trait.impl/core/clone/trait.Clone.js
+++ docs/trait.impl/core/clone/trait.Clone.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["pescan",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"pescan/output/enum.Format.html\" title=\"enum pescan::output::Format\">Format</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"pescan/cache/struct.Api.html\" title=\"struct pescan::cache::Api\">Api</a>"]]]]);
+    var implementors = Object.fromEntries([["pescan",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"pescan/output/enum.Format.html\" title=\"enum pescan::output::Format\">Format</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[515]}
+//{"start":57,"fragment_lengths":[266]}
